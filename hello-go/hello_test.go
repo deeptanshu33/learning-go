01e4d8f513f9a47b6ac955d5727b66313b8aa0e2
Deeptanshu Prasad <Deeptanshu.me@outlook.com>
2026-06-27 12:12:36 +0530

    init + hello_world

---
+++ hello-go/hello_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Frank", "French")
+		want := "Bonjour, Frank"
+		assertCorrectMessage(t, got, want)
+	})
+}
---
+++ hello-go/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func getPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	prefix := getPrefix(language)
+
+	return prefix + name
+}
+
+func main() {
+	fmt.Println(Hello("Chris", ""))
+}
